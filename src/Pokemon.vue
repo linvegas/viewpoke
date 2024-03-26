308700b8e980e17e3d1a1ff12d3dcaf31a3f4b6c
linvegas <saulo.costam@gmail.com>
2024-03-26 11:55:05 -0300

    Setting up basic layout for pokemon page

--- src/Pokemon.vue
+++ src/Pokemon.vue
@@ -7,6 +7,11 @@ type PokeData = {
   name: string;
   sprites: {
     front_default: string;
+    other: {
+      "official-artwork": {
+        front_default: string;
+      }
+    }
   };
   stats: {
     base_stat: number;
@@ -21,7 +26,17 @@ type PokeData = {
   }[];
 };
 
+type PokeSpecie = {
+  flavor_text_entries: {
+    flavor_text: string;
+    language: {
+      name: string;
+    }
+  }[]
+}
+
 const poke = ref<PokeData>();
+const poke_specie = ref<PokeSpecie>();
 
 const route = useRoute();
 
@@ -30,12 +45,82 @@ watch(() => route.params.name, fetch_pokemon, {immediate: true})
 async function fetch_pokemon(name: string) {
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
   poke.value = await res.json();
+  const res2 = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${poke.value.id}`);
+  poke_specie.value = await res2.json();
 }
+
+const captitalize = (word: string) => word.charAt(0).toUpperCase() + word.slice(1)
+
 </script>
 
 <template>
-  <h1>Pokemon Page</h1>
-  <p>{{ poke?.name }}</p>
-  <p>{{ poke?.id }}</p>
-  <img :src="poke?.sprites.front_default" atl="Srite" />
+  <header>
+    <div class="squeeze flex">
+      <h1>
+        <RouterLink to="/">PokeView</RouterLink>
+      </h1>
+      <input type="text" :placeholder="poke?.name" />
+    </div>
+  </header>
+  <main v-if="poke" class="squeeze">
+    <section class="flex">
+      <img :src="poke.sprites.other['official-artwork'].front_default" alt="Srite" />
+      <article>
+        <h2>{{ captitalize(poke.name) }} #{{ poke.id }}</h2>
+        <p id="types">
+          <span class="type-card" v-for="t in poke.types">{{ t.type.name }}</span>
+        </p>
+        <p v-if="poke_specie">
+          {{
+            poke_specie.flavor_text_entries
+              .filter(text => text.language.name === "en")[0]
+              .flavor_text
+          }}
+        </p>
+      </article>
+    </section>
+  </main>
 </template>
+
+<style scoped>
+  .squeeze {
+    width: min(100% - 3rem, 1250px);
+    margin-inline: auto;
+  }
+  .flex {
+    display: flex;
+    gap: 1rem;
+  }
+  header {
+    border-bottom: 1px solid grey;
+    padding-block: 1rem;
+    & > div.flex {
+      flex-wrap: wrap;
+      gap: 0.5rem;
+      justify-content: space-between;
+    }
+  }
+  section.flex {
+    flex-wrap: wrap;
+    align-items: center;
+    gap: 2rem;
+    & > * {
+      flex-shrink: 1;
+      flex-basis: 10ch;
+    }
+    & > article {
+      flex-grow: 2;
+    }
+  }
+  p#types {
+    padding-block: 0.5rem;
+    & > * {
+      margin-right: 0.5rem;
+    }
+  }
+  span.type-card {
+    padding: 0.25rem;
+    border: 1px solid grey;
+    border-radius: 0.5rem;
+  }
+</style>
